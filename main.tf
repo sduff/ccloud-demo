0990bb0ac52b775af0a103da6ecb4b099db3c861
Simon Duff <simon.duff@gmail.com>
2023-01-30 17:42:19 +1100

    Create new environment

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ terraform {
   }
 }
 
+# Confluent Cloud API Key variables, stored in Terraform Cloud Variable Sets
 variable "confluent_cloud_api_key" {
   description = "Confluent Cloud API Key (also referred as Cloud API ID)"
   type        = string
@@ -18,49 +19,13 @@ variable "confluent_cloud_api_secret" {
   sensitive   = true
 }
 
+# Confluent Terraform Provider
 provider "confluent" {
   cloud_api_key    = var.confluent_cloud_api_key
   cloud_api_secret = var.confluent_cloud_api_secret
 }
 
-# Existing Environment, "team"
-data "confluent_environment" "env" {
-  display_name = "team"
-}
-
-# Existing service account, "tf-svc-acct", with OrgAdmin
-data "confluent_service_account" "sa-org" {
-  display_name = "tf-svc-acct"
-}
-
-resource "confluent_api_key" "tf-svc-acct-api-key" {
-  display_name = "tf-svc-acct-api-key"
-  description  = "TF Service Account API Key"
-  owner {
-    id          = data.confluent_service_account.env-manager.id
-    api_version = data.confluent_service_account.env-manager.api_version
-    kind        = data.confluent_service_account.env-manager.kind
-  }
-
-  managed_resource {
-    id          = confluent_kafka_cluster.basic.id
-    api_version = confluent_kafka_cluster.basic.api_version
-    kind        = confluent_kafka_cluster.basic.kind
-
-    environment {
-      id = data.confluent_environment.staging.id
-    }
-  }
-}
-
-resource "confluent_kafka_topic" "topic0" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.basic.id
-  }
-  topic_name    = "orders"
-  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
-  credentials {
-    key    = confluent_api_key.env-manager-kafka-api-key.id
-    secret = confluent_api_key.env-manager-kafka-api-key.secret
-  }
+# Create a new environment
+resource "confluent_environment" "env" {
+  display_name = "sduff-new-env"
 }
