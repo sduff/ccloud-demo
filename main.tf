b7690603fb5c521f1916990e557768af550d9f92
Simon Duff <simon.duff@gmail.com>
2023-02-13 11:04:24 +1100

    Azure Resource Group

--- main.tf
+++ main.tf
@@ -231,7 +231,8 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "apac-ps-ccloud-rg" {
+# Create a resource Group
+resource "azurerm_resource_group" "apac_ps_ccloud_rg" {
   name ="apac-ps-ccloud-rg"
   location = "australiasoutheast"
   tags = {
@@ -239,3 +240,40 @@ resource "azurerm_resource_group" "apac-ps-ccloud-rg" {
   }
 }
 
+# Create a KeyVault
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "keyvault" {
+  depends_on                  = [azurerm_resource_group.apac_ps_ccloud_rg]
+  name                        = "keyvault-apac-ps-ccloud"
+  location                    = azurerm_resource_group.apac_ps_ccloud_rg.location
+  resource_group_name         = azurerm_resource_group.apac_ps_ccloud_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  }
+
+  tags = {
+    owner_email = "sduff@confluent.io"
+  }
+
+}
