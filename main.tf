2d2945ea5aa1600f6560ff50e84fe0750308fd7d
Simon Duff <simon.duff@gmail.com>
2023-04-24 17:07:09 +1000

    Policy As Code Test Terraform Plan

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+#  _______       __ __            _______       _______          __
+# |   _   .-----|  |__.----.--.--|   _   .-----|   _   .-----.--|  .-----.
+# |.  1   |  _  |  |  |  __|  |  |.  1   |__ --|.  1___|  _  |  _  |  -__|
+# |.  ____|_____|__|__|____|___  |.  _   |_____|.  |___|_____|_____|_____|
+# |:  |                    |_____|:  |   |     |:  1   |
+# |::.|                          |::.|:. |     |::.. . |
+# `---'---               __  ____'--- ---'     `-------'     ___                       ______ __
+# |_     _|.-----.-----.|  ||_     _|.-----.----.----.---.-.'  _|.-----.----.--------.|   __ \  |.---.-.-----.
+#   |   |  |  -__|__ --||   _||   |  |  -__|   _|   _|  _  |   _||  _  |   _|        ||    __/  ||  _  |     |
+#   |___|  |_____|_____||____||___|  |_____|__| |__| |___._|__|  |_____|__| |__|__|__||___|  |__||___._|__|__|
+
 terraform {
   required_providers {
     confluent = {
@@ -34,12 +45,26 @@ data "confluent_environment" "development" {
   display_name = "Terraform-Environment"
 }
 
-# dedicated
+data "confluent_user" "user-account" {
+  display_name = "sduff"
+}
+
+#
+# ----------------------------------------#
+# Terraform plan for as many bad policies #
+# ----------------------------------------#
+#
+
+# Cluster
+# - non approved cloud
+# - non approved region
+# - dedicated
+
 resource "confluent_kafka_cluster" "dedicated" {
-  display_name = "dedicated_kafka_cluster_3"
+  display_name = "bad_dedicated_kafka_cluster"
   availability = "MULTI_ZONE"
-  cloud        = "AWS"
-  region       = "us-east-2"
+  cloud        = "GCP"
+  region       = "europe-central2-a"
   dedicated {
     cku = 2
   }
@@ -53,19 +78,121 @@ resource "confluent_kafka_cluster" "dedicated" {
   }
 }
 
-# basic
-resource "confluent_kafka_cluster" "basic" {
-  display_name = "basic_kafka_cluster"
-  availability = "SINGLE_ZONE"
-  cloud        = "AWS"
-  region       = "us-east-2"
-  basic {}
+# API Key
+# - has invalid name
+# - owner is a user, not a service account
 
-  environment {
-    id = data.confluent_environment.development.id
+resource "confluent_api_key" "user-api-key" {
+  display_name = "bad-policy-user-api-key"
+  description  = "Bad Policy User API Key"
+  owner {
+    id          = data.confluent_user.user-account.id
+    api_version = data.confluent_user.user-account.api_version
+    kind        = data.confluent_user.user-account.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = data.confluent_environment.development.id
+    }
   }
 
   lifecycle {
     prevent_destroy = true
   }
 }
+
+# Cluster Config
+# - enable auto.create.topics.enable
+
+resource "confluent_kafka_cluster_config" "config" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  config = {
+    "auto.create.topics.enable" = "true"
+  }
+  credentials {
+    key    = confluent_api_key.user-api-key.id
+    secret = confluent_api_key.user-api-key.secret
+  }
+}
+
+# Create a Service Account
+# - invalid name
+
+resource "confluent_service_account" "bad-sa" {
+  display_name = "bad-policy-service-account"
+  description  = "Bad Policy Service Account"
+}
+
+# Create RBAC
+# - invalid role
+
+resource "confluent_role_binding" "rb" {
+  principal   = "User:${data.confluent_user.user-account.id}"
+  role_name   = "OrganizationalAdmin"
+  crn_pattern = confluent_environment.dedicated.resource_name
+}
+
+# Create Topic 1
+# - invalid topic name
+# - lower partition count
+# - config retention.ms
+
+resource "confluent_kafka_topic" "topic01" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  topic_name         = "topic01"
+  partitions_count   = 1
+  rest_endpoint      = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.user-api-key.id
+    secret = confluent_api_key.user-api-key.secret
+  }
+  config = {
+    "retention.ms"                        = "999999999"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Create Topic 2
+# - invalid topic name
+# - high partition count
+# - config retention.bytes
+
+resource "confluent_kafka_topic" "topic02" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  topic_name         = "topic02"
+  partitions_count   = 25
+  rest_endpoint      = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.user-api-key.id
+    secret = confluent_api_key.user-api-key.secret
+  }
+  config = {
+    "retention.bytes"                     = "1073741824"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Attempt to create a non-approved resource
+
+resource "confluent_invitation" "bad-invite" {
+      email = "hacker@example.com"
+}
+
+# :(
+# No resource for testing "preventing the deletion of topics"
